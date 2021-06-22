e1bb5b09c0d16487d91f027ab98e04d68d1c163f
luck_nhb <luck_nhb@163.com>
2021-06-22 09:59:55 +0800

    RSA加密算法

---
+++ RsaUtil.java
@@ -0,0 +1,106 @@
+package com.cloudcare.cbis.security.biz.utils;
+
+import com.cloudcare.common.lang.security.PasswordHelper;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author luck_nhb
+ * @Description
+ * @create 2021-06-15 15:56
+ */
+public class RsaUtil {
+    /**
+     * 密钥长度 于原文长度对应 以及越长速度越慢
+     */
+    private final static int KEY_SIZE = 1024;
+    /**
+     * 用于封装随机产生的公钥与私钥
+     */
+    private static Map<Integer, String> keyMap = new HashMap<Integer, String>();
+
+    /**
+     * 随机生成密钥对
+     */
+    public static void genKeyPair() throws NoSuchAlgorithmException {
+        // KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        // 初始化密钥对生成器
+        keyPairGen.initialize(KEY_SIZE, new SecureRandom());
+        // 生成一个密钥对，保存在keyPair中
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        // 得到私钥
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        // 得到公钥
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        String publicKeyString = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+        // 得到私钥字符串
+        String privateKeyString = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+        // 将公钥和私钥保存到Map
+        //0表示公钥
+        keyMap.put(0, publicKeyString);
+        //1表示私钥
+        keyMap.put(1, privateKeyString);
+    }
+
+    /**
+     * RSA公钥加密
+     *
+     * @param str    加密字符串
+     * @param publicKey 公钥
+     * @return 密文
+     * @throws Exception 加密过程中的异常信息
+     */
+    public static String encrypt(String str, String publicKey) throws Exception {
+        //base64编码的公钥
+        byte[] decoded = Base64.getDecoder().decode(publicKey);
+        RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
+        //RSA加密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        String outStr = Base64.getEncoder().encodeToString(cipher.doFinal(str.getBytes("UTF-8")));
+        return outStr;
+    }
+
+    /**
+     * RSA私钥解密
+     *
+     * @param str    加密字符串
+     * @param privateKey 私钥
+     * @return 明文
+     * @throws Exception 解密过程中的异常信息
+     */
+    public static String decrypt(String str, String privateKey) throws Exception {
+        //64位解码加密后的字符串
+        byte[] inputByte = Base64.getDecoder().decode(str);
+        //base64编码的私钥
+        byte[] decoded = Base64.getDecoder().decode(privateKey);
+        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+        //RSA解密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, priKey);
+        String outStr = new String(cipher.doFinal(inputByte));
+        return outStr;
+    }
+
+    public static void main(String[] args) throws Exception {
+        /*long temp = System.currentTimeMillis();
+        //生成公钥和私钥
+        String privateKey = Configs.getString("rsa.privateKey");
+        //通过密文，和私钥解密。
+        String result = "aifSaAxq/4JrOVpHhQ9phVNbCPkTJNJqSMUS3Q5uMgT/wiwbqj+qpMBHWt+Rl8JW05Kx3ujht5MOmE1r1beY6+1LGQksTA35aTrt382/0kW4DXjl8lCOk4U5M1QP/6TOKqC5tMh06Ic9XFK7hELIxmnI8ySYBZC7jgQrdCUcBVw=";
+        String messageDe = decrypt(result, privateKey);
+        System.out.println("解密:" + messageDe);
+        System.out.println("解密消耗时间:" + (System.currentTimeMillis() - temp) / 1000.0 + "秒");*/
+        boolean equals = PasswordHelper.encryption("Cbis123!@#").equals("72711c3972dedb6d742cd12184ac9d53");
+        System.out.println(equals);
+    }
+}
